73a8315c6b2d6b4866a3d300aaafd377f30fbb58
LCherryholmes <lcherryh@yahoo.com>
2026-03-17 07:40:16 +0000

    license: replace EPL-2.0 with AGPL-3.0; update project.clj; add README badge

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SNOBOL4 in Clojure
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject SNOBOL4clojure "0.2.0"
   :description "SNOBOL4 pattern matching."
   :url ""
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "AGPL-3.0-or-later"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
     [org.clojure/clojure "1.10.3"]
     [org.clojure/tools.trace "0.7.11"]
